861a147d44aeb47a127f8f28770b3d2ab99bd069
Konstantin Haehnelt <konstantinhaehnelt@Martin-Haehnelts-MacBook-Pro.local>
2016-03-31 19:02:05 +0100

    Commit message

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Baecoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101bed402a9ad1f8f1560dc2213bf6b337485a074c9cf36bf220c5d1e98dceaf697";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
